3795d3297a546defd598891c08002e5765e59575
quentinRnd <78441186+quentinRnd@users.noreply.github.com>
2023-10-18 17:50:23 +0200

    Update main.cpp to run his document root in static_files

--- app/source/main.cpp
+++ app/source/main.cpp
@@ -1,14 +1,13 @@
 #include <drogon/HttpAppFramework.h>
-
 int main() 
 {
     // Adress: http://localhost:8000/ 
     const std::string ip("0.0.0.0");
     const uint16_t port(8000);
     drogon::app().addListener(ip, port);
+    drogon::app().setDocumentRoot("static_files");
     LOG_INFO << "Server running on " << ip << ":" << port;
-
     // Run the app
-    drogon::app().setDocumentRoot("../source").run();
+    drogon::app().run();
     return EXIT_SUCCESS;
 }
