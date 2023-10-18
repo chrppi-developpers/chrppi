d01e0e4d14730d195ffec10d55b72ce8b38610b8
quentinRnd <78441186+quentinRnd@users.noreply.github.com>
2023-10-18 17:50:58 +0200

    Update Index.cpp

--- app/source/controllers/Index.cpp
+++ app/source/controllers/Index.cpp
@@ -1,25 +1,13 @@
-#include <drogon/HttpSimpleController.h>
-#include <drogon/HttpResponse.h>
+#include"Index.hh"
 
-class Index: 
-	public drogon::HttpSimpleController<Index>
+void Index::asyncHandleHttpRequest
+(
+    const drogon::HttpRequestPtr &req,
+    std::function<void(const drogon::HttpResponsePtr &)> &&callback
+) 
 {
-  public:
-
-    PATH_LIST_BEGIN
-    PATH_ADD("/", drogon::Get);
-    PATH_LIST_END
-
-    void asyncHandleHttpRequest
-	(
-        const drogon::HttpRequestPtr &req,
-        std::function<void(const drogon::HttpResponsePtr &)> &&callback
-	) override
-    {
-        drogon::HttpViewData data;
-        data["name"] = req->getParameter("name");
-        auto resp = drogon::HttpResponse::newHttpViewResponse("index_view", data);
-
-        callback(resp);
-    }
-};
+    drogon::HttpViewData data;
+    data["name"] = req->getParameter("name");
+    auto resp = drogon::HttpResponse::newHttpViewResponse("index_view", data);
+    callback(resp);
+}
